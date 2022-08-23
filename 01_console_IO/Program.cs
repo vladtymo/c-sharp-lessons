9eec161023cda1b8f20404018d0c7a88ab6d186c
Vlad Tymoshchuk <59306514+vladtymo@users.noreply.github.com>
2022-08-23 13:32:59 +0300

    Console IO examples.

---
+++ 01_console_IO/Program.cs
@@ -0,0 +1,22 @@
+﻿// it is a line comment
+
+/*
+    block comment
+*/
+
+Console.WriteLine("Console IO example project.");
+
+Console.Write("Some text with Write method!");         // without end of line
+Console.WriteLine("Some text with WriteLine method!"); // with end of line
+
+/* Escape sequences: 
+ * \n - end of line
+ * \t - tabulation
+ * \" - "
+ * \' - '
+ * \\ - \
+ */
+Console.Write("First.\n\tSecond.\n\t\tThird.\n");
+Console.WriteLine("My name is \"Vlad\"");
+Console.WriteLine("Url: http:\\\\www.google.com");
+
