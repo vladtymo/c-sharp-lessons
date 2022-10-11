c0e4c8678e58206c040e31e2d6449c1e2da86fdd
Vlad Tymoshchuk <59306514+vladtymo@users.noreply.github.com>
2022-10-11 11:56:19 +0300

    Cycle for.

---
+++ 07_cycles/Program.cs
@@ -0,0 +1,26 @@
+﻿// while
+// do while
+// for
+
+/*
+    counter = 0; - create counter
+    counter++;   - condition
+    counter < 5  - expression
+
+    for (counter; condition; expression)
+    {
+        ...code...
+    }
+ */
+
+Console.Write("Enter counter: ");
+int number = int.Parse(Console.ReadLine());
+
+for (int counter = 0; counter < number; counter++)
+{
+    Console.WriteLine("----------------");
+    Console.WriteLine("Have a good day!");
+    Console.WriteLine("----------------");
+}
+
+Console.WriteLine("End!");
