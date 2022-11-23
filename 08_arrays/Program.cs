6369a36ea4f2c1f3ed4ea36fbe985fcc742befdc
Vlad Tymoshchuk <59306514+vladtymo@users.noreply.github.com>
2022-11-23 12:00:55 +0200

    Intro to Arrays.

---
+++ 08_arrays/Program.cs
@@ -0,0 +1,24 @@
+﻿// create array template: type[] name = new type[size];
+
+// create array of prices
+double[] prices = new double[1000];
+
+// set element value
+prices[0] = 1300.5;
+prices[1] = 7800.9;
+prices[2] = 999;
+
+prices[7] = 550090.45;
+
+Console.WriteLine($"First product price = {prices[0]}");
+Console.WriteLine($"Last product price = {prices[9]}");
+
+// show all prices
+
+for (int i = 0; i < 1000; ++i)
+{
+    Console.WriteLine($"Product {i+1} price = {prices[i]}");
+}
+
+
+
