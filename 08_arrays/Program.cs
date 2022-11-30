8a7e7f2f3ec1b624e81d03260131d15fa564b7b6
Vlad Tymoshchuk <59306514+vladtymo@users.noreply.github.com>
2022-11-30 18:07:55 +0200

    Arrays with cycles.

--- 08_arrays/Program.cs
+++ 08_arrays/Program.cs
@@ -1,7 +1,7 @@
 ﻿// create array template: type[] name = new type[size];
 
 // create array of prices
-double[] prices = new double[1000];
+double[] prices = new double[10];
 
 // set element value
 prices[0] = 1300.5;
@@ -10,15 +10,42 @@ prices[2] = 999;
 
 prices[7] = 550090.45;
 
-Console.WriteLine($"First product price = {prices[0]}");
-Console.WriteLine($"Last product price = {prices[9]}");
+//Console.WriteLine($"First product price = {prices[0]}");
+//Console.WriteLine($"Last product price = {prices[9]}");
 
 // show all prices
 
-for (int i = 0; i < 1000; ++i)
+//for (int i = 0; i < 10; ++i)
+//{
+//    Console.WriteLine($"Product {i + 1} price = {prices[i]}");
+//}
+
+// Завдання: зберегти та відобразити список кольорів
+
+// ініціалізація масива
+//string[] colors = new string[5] { "red", "green", "blue", "gray", "white" };
+
+//Console.Write("Enter color: ");
+//colors[0] = Console.ReadLine();
+
+//Console.WriteLine("Colors: " + colors.Length);
+
+//// indexes: 0 ... 4
+
+//for (int index = 0; index < colors.Length; ++index)
+//{
+//    Console.Write($"{colors[index]}, ");
+//}
+
+// Завдання: обрахувати загальну ціну всіх товарів
+double[] costs = new double[8] { 100, 55, 120.5, 1000, 12, 99, 300, 550 };
+
+Console.Write("Costs: ");
+
+float summ = 0;
+
+for (int i = 0; i < costs.Length; ++i)
 {
-    Console.WriteLine($"Product {i+1} price = {prices[i]}");
+    costs[i]
+    Console.Write($"{costs[i]} ");
 }
-
-
-
